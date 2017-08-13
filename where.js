f3a61f1fbe19e6483658ab61bce8c75488fbba2b
jackhowa <johncarverhoward@gmail.com>
2017-08-13 15:04:11 -0500

    pseudocode the sorting algorithm

--- where.js
+++ where.js
@@ -1,7 +1,16 @@
 
 function getIndexToIns(arr, num) {
-  // Find my place in this sorted array.
-  return num;
+  // sort the array 
+  // define empty index variable 
+  // loop through the array
+    // compare the target num with indexed array
+    // if target num is equal or greater than indexed array value 
+      // set the target index var to the i 
+    // elsif target num is less than indexed array val
+      // return the target index var 
+      // or possibly could just return the i - 1 
+    // end 
+    // return the target index var 
 }
 
 getIndexToIns([40, 60], 50);
