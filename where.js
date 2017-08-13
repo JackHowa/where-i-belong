6808ca98489f1d0b8ff3ce9d0a856b58811d8506
jackhowa <johncarverhoward@gmail.com>
2017-08-13 15:36:28 -0500

    refactor and remove unnec redeclaration of var

--- where.js
+++ where.js
@@ -1,24 +1,26 @@
 
 function getIndexToIns(arr, num) {
   // sort the array 
-  var sortedArr = arr.sort(); 
-
-  // define empty index variable 
-  var targetIndex = 0; 
+  // .sort() doesn't work with integers 
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  // arr sort is destructive; don't need to redefine var 
+  arr.sort(function(a, b) {
+    return a - b;
+  });
 
   // loop through the array
-  for (var i = 0; i < sortedArr.length; i++) {
+  for (var i = 0; i < arr.length; i++) {
     // compare the target num with indexed array
-    // if target num is equal or greater than indexed array value 
+    // if indexed array value  is equal or greater than target num
 
-    if (num < sortedArr[i]) {
-      // set the target index var to the i 
+    if (num <= arr[i]) {
+      // then that index is either the right one or last right one 
       return i;
     } 
   }
 
-  // return the target index var if target index is last 
-  return targetIndex;
+  // return the length of array if target index is last 
+  return arr.length;
 }
 
-console.log(getIndexToIns([10, 20, 30, 40, 50], 35));
+console.log(getIndexToIns([5, 3, 20, 3], 5));
